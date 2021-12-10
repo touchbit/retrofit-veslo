5374a239de4e0df01d4d7d77e46608a1e4076408
Oleg Shaburov <shaburov.o.a@gmail.com>
2021-12-11 01:29:45 +0300

    BaseCallAdapterFactory Tests

--- core/src/main/java/org/touchbit/retrofit/ext/dmr/client/adapter/BaseCallAdapterFactory.java
+++ core/src/main/java/org/touchbit/retrofit/ext/dmr/client/adapter/BaseCallAdapterFactory.java
@@ -30,15 +30,41 @@ import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 
+/**
+ * Base extension call adapter factory {@link CallAdapter.Factory}
+ * Implements the basic methods for getting the response body model
+ * <p>
+ * Created by Oleg Shaburov on 11.12.2021
+ * shaburov.o.a@gmail.com
+ */
 public abstract class BaseCallAdapterFactory extends CallAdapter.Factory {
 
+    /**
+     * Slf4J logger
+     */
     protected final Logger logger;
 
+    /**
+     * @param logger - required Slf4J logger
+     */
     protected BaseCallAdapterFactory(Logger logger) {
         Utils.parameterRequireNonNull(logger, "logger");
         this.logger = logger;
     }
 
+    /**
+     * Force calls a typed converter if HTTP status code 204/205 or {@link Response#body()} return null
+     * This ensures that the request body is processed correctly for the {@link Type}.
+     * If an exception was thrown and method {@link Response#isSuccessful()} returned false,
+     * then null will be returned without throwing an exception.
+     *
+     * @param response    - Retrofit call response
+     * @param returnType  - Successful model {@link Type}
+     * @param annotations - list of annotations for the called API method
+     * @param retrofit    - HTTP client
+     * @return Successful model
+     * @throws ConvertCallException {@link ConvertCallException} If the body of a successful response is not convertible
+     */
     @Nullable
     @SuppressWarnings("ConstantConditions")
     protected Object getSuccessfulResponseBody(final @Nonnull Response<?> response,
@@ -67,7 +93,7 @@ public abstract class BaseCallAdapterFactory extends CallAdapter.Factory {
             if (response.isSuccessful()) {
                 throw e;
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             if (response.isSuccessful()) {
                 throw new ConvertCallException("Error converting response body to type " + returnType.getTypeName(), e);
             }
@@ -75,6 +101,18 @@ public abstract class BaseCallAdapterFactory extends CallAdapter.Factory {
         return null;
     }
 
+    /**
+     * Converting  {@link Response#errorBody()} to an error model {@link Type}.
+     * If an exception was thrown and method {@link Response#isSuccessful()} returned true,
+     * then null will be returned without throwing an exception.
+     *
+     * @param response    - Retrofit call response
+     * @param returnType  - Error model {@link Type}
+     * @param annotations - list of annotations for the called API method
+     * @param retrofit    - HTTP client
+     * @return Error model
+     * @throws ConvertCallException If the body of a error response is not convertible
+     */
     @Nullable
     @SuppressWarnings("ConstantConditions")
     protected Object getErrorResponseBody(final @Nonnull Response<?> response,
@@ -98,6 +136,11 @@ public abstract class BaseCallAdapterFactory extends CallAdapter.Factory {
         return dto;
     }
 
+    /**
+     * @param returnType - model type
+     * @param body - model value
+     * @throws PrimitiveConvertCallException if returnType is primitive and body is null
+     */
     protected void checkPrimitiveConvertCall(final @Nonnull Type returnType, final @Nullable Object body) {
         logger.debug("Checking that the primitive is not null");
         if (returnType instanceof Class && ((Class<?>) returnType).isPrimitive() && body == null) {
